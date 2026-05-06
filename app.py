d44ec5ad7de4b2de039bf12ecc69e959e67beaa5
osvaldoadelino10-alt <osvaldoadelino10@gmail.com>
2026-05-06 11:57:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify #
+from flask import Flask, request, jsonify 
 from twilio.twiml.messaging_response import MessagingResponse
 from datetime import datetime
 import json
